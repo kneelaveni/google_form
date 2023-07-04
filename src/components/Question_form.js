45526385611be218caa1ac768e925c795c70341b
kneelaveni <kneelaveni17@gmail.com>
2023-07-04 17:52:23 +0530

    add icons dynamically

--- src/components/Question_form.js
+++ src/components/Question_form.js
@@ -37,6 +37,72 @@ function Question_form() {
       required: false,
     },
   ]);
+  function changeQuestion(text,i){
+    var newQuestion = [...questions];
+    newQuestion[i].questionText = text;
+    setQuestions(newQuestion);
+    console.log(newQuestion);
+  }
+  
+  function changeOptionValue(text,i,j){
+    var optionQuestion = [...questions];
+    optionQuestion[i].options[j].optionText = text;
+
+    setQuestions(optionQuestion);
+  }
+  function addQuestionType(i,type){
+   let qs=[...questions];
+   console.log(type);
+   qs[i].questionType=type;
+   setQuestions(qs);
+  }
+
+  function removeOption(i,j){
+    var RemoveOptionQuestion = [...questions];
+    if(RemoveOptionQuestion[i].options.length > 1){
+      RemoveOptionQuestion[i].options.splice(j,1);
+      setQuestions(RemoveOptionQuestion)
+      console.log(i+"_"+j)
+    }
+  }
+
+  function addOption(i){
+    var optionsOfQuestion = [...questions];
+    if (optionsOfQuestion[i].options.length < 5){
+      optionsOfQuestion[i].options.push({optionText:"option "+(optionsOfQuestion[i].options.length + 1)})
+      console.log(optionsOfQuestion)
+    }
+    else{
+      console.log("Max 5 options")
+    }
+    setQuestions(optionsOfQuestion);
+  }
+
+  function copyQuestion(i){
+    // expandCloseAll()
+    let qs = [...questions]
+    var newQuestion=qs[i]
+    setQuestions([...questions,newQuestion])
+  }
+  
+  function deleteQuestion(i){
+    let qs = [...questions]
+    if(questions.length > 1){
+      qs.splice(i,1);
+    }
+    setQuestions(qs)
+  }
+
+  function requiredQuestion(i){
+    var reqQuestion = [...questions]
+    reqQuestion[i].required = !reqQuestion[i].required
+    console.log(reqQuestion[i].required +" "+i);
+    setQuestions(reqQuestion)
+  }
+
+  function addMoreQuestionField(){
+    setQuestions([...questions,{questionText:"Question",questionType:"radio",options:[{optionText:"Option 1"}],open:true,required:false}]);
+  }
 
   function questionsUI() {
     return questions.map((ques, i) => (
@@ -44,7 +110,7 @@ function Question_form() {
         expanded={questions[i].open}
         className={questions[i].open ? "add_border" : ""}
       >
-        <AccordionSummary
+        {/* <AccordionSummary
           aria-controls="panelia-content"
           id="panela-hearder"
           elavation={1}
@@ -99,7 +165,7 @@ function Question_form() {
           ) : (
             ""
           )}
-        </AccordionSummary>
+        </AccordionSummary> */}
         <div className="question_boxes">
           <AccordionDetails className="add_question">
             <div className="add_question_top">
@@ -108,27 +174,30 @@ function Question_form() {
                 className="question"
                 placeholder="Question"
                 value={ques.questionText}
+                onChange={(e)=>{changeQuestion(e.target.value,i)}}
               />
               <CropOriginalIcon style={{ color: "#5f6368" }} />
               <Select
                 className="select"
                 style={{ color: "#5f6368", fontSize: "13px" }}
               >
-                <MenuItem id="text" value="Text" className="menuitem">
+                <MenuItem id="text" value="Text" className="menuitem" onClick={()=>{addQuestionType(i,"text")}}>
                   <SubjectIcon style={{ marginRight: "10px" }} />
                   Paragraph
                 </MenuItem>
-                <MenuItem id="checkbox" value="Checkbox" className="menuitem">
+                <MenuItem id="checkbox" value="Checkbox" className="menuitem"  onClick={()=>{addQuestionType(i,"checkbox")}}>
                   <CheckBoxIcon
                     style={{ marginRight: "10px", color: "#70757a" }}
                     checked
+                   
                   />
                   Checkbox
                 </MenuItem>
-                <MenuItem id="radio" value="Radio" className="menuitem">
+                <MenuItem id="radio" value="Radio" className="menuitem" onClick={()=>{addQuestionType(i,"radio")}}>
                   <Radio
                     style={{ marginRight: "10px", color: "#70757a" }}
                     checked
+                    
                   />
                   Multiple Choice
                 </MenuItem>
@@ -136,7 +205,7 @@ function Question_form() {
             </div>
             {ques.options.map((op, j) => (
               <div className="add_question_body" key={j}>
-                {ques.questionType != "text" ? (
+                {ques.questionType !== "text" ? (
                   <input
                     type={ques.questionType}
                     style={{ marginRight: "10px" }}
@@ -150,17 +219,35 @@ function Question_form() {
                     className="text_input"
                     placeholder="option"
                     value={ques.options[j].optionText}
+                    onChange={(e)=>{changeOptionValue(e.target.value,i,j)}}
                   />
                 </div>
                 <CropOriginalIcon style={{ color: "#5f6368" }} />
                 <IconButton aria-label="delete">
-                  <CloseIcon />
+                  <CloseIcon onClick={()=>{removeOption(i,j)}}/>
                 </IconButton>
               </div>
             ))}
+            {ques.options.length < 5 ?(
+              <div className="add_question_body">
+                <FormControlLabel disabled control={
+                  (ques.questionType!=="text")?
+                  <input type={ques.questionType} color="primary" inputProps={{'aria-lable':'secondary checkbox'}}
+                  style={{marginLeft:"10px",marginRight:"10px"}} disabled/> :
+                  <ShortTextIcon style ={{marginRight:"10px"}}/>
+
+                } label={
+                  <div>
+                    <input type="text" className="text_input" style={{fontSize:"13px",width:"60px"}} placeholder="Add other"/>
+                    <Button size="small" onClick={()=>{addOption(i)}} style={{textTransform:"none",color:"#4285f4",fontSize:"13px",fontWeight:"600"}} >Add Option</Button>
+                  </div>
+
+                }/>
+
+              </div>
+            ):""}
             <div className="add_footer">
               <div className="add_question_bottom_left">
-               
                 <Button
                   size="small"
                   style={{
@@ -182,18 +269,27 @@ function Question_form() {
               </div>
               <div className="add_question_bottom">
                 <IconButton aria-label="Copy">
-                    <FilterNoneIcon/>
+                  <FilterNoneIcon onClick={()=>{copyQuestion(i)}}/>
                 </IconButton>
                 <IconButton aria-label="delete">
-                    <BsTrash/>
+                  <BsTrash onClick={()=>{deleteQuestion(i)}}/>
                 </IconButton>
-                <span style={{color:"#5f6368",fontSize:'13px'}}>Required</span><Switch name="checkedA" color="primary" checked/>
-                 <IconButton>
-                    <MoreVertIcon/>
-                 </IconButton>
+                <span style={{ color: "#5f6368", fontSize: "13px" }}>
+                  Required
+                </span>
+                <Switch name="checkedA" color="primary"  onClick={()=>{requiredQuestion(i)}}/>
+                <IconButton>
+                  <MoreVertIcon />
+                </IconButton>
               </div>
             </div>
           </AccordionDetails>
+          <div className="question_edit">
+            <AddCircleOutlineIcon className="edit" onClick={addMoreQuestionField}/>
+            <OndemandVideoIcon className="edit" />
+            <CropOriginalIcon className="edit" />
+            <TextFieldsIcon className="edit" />
+          </div>
         </div>
       </Accordion>
     ));
